006af1fac172b2b512828632e6717d752c04e908
Jessi Pavia <jessi316866@gmail.com>
2024-06-04 19:03:22 -0400

    video feature correctly added to mux

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,7 +1,13 @@
-import Mux from "@mux/mux-node";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import Mux from '@mux/mux-node';
+import test from "node:test";
+
+const mux = new Mux({
+    tokenId: process.env.MUX_TOKEN_ID,
+    tokenSecret: process.env.MUX_TOKEN_SECRET,
+});
 
 export async function PATCH(
     req: Request,
@@ -35,6 +41,38 @@ export async function PATCH(
                 ...values,
             }
         });
+
+
+        if (values.videoUrl) {
+            const existingMuxData = await db.muxData.findFirst({
+                where: {
+                    chapterId: params.chapterId,
+                }
+            });
+
+            if (existingMuxData) {
+                await mux.video.assets.delete(existingMuxData.assetId);
+                await db.muxData.delete({
+                    where: {
+                        id: existingMuxData.id,
+                    }
+                });
+            }
+
+            const asset = await mux.video.assets.create({
+                input: values.videoUrl,
+                playback_policy: ['public'],
+                test: false,
+            });
+
+            await db.muxData.create({
+                data: {
+                    chapterId: params.chapterId,
+                    assetId: asset.id,
+                    playbackId: asset.playback_ids?.[0]?.id,
+                }
+            });
+        }
 
         
         return NextResponse.json(chapter);
