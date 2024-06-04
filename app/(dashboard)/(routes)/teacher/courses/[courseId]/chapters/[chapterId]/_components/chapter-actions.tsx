662097ccab8668a1f986653d2db0ea5307e4c98b
Jessi Pavia <jessi316866@gmail.com>
2024-06-04 19:36:24 -0400

    Pop up window Are you sure?

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+
+//Create an interface that is gonna match the props
+interface ChapterActionsProps {
+    disabled: boolean,
+    courseId: string,
+    chapterId: string,
+    isPublished: boolean,
+};
+
+export const ChapterActions =({
+    disabled,
+    courseId,
+    chapterId,
+    isPublished
+}: ChapterActionsProps) => {
+    return(
+        <div className="flex items-center gap-x-2">
+            <Button
+                onClick={() => {}}
+                disabled={disabled}
+                variant="outline"
+                size="sm"
+            >
+                {isPublished ? "Unpublish": "Publish"}
+            </Button>
+            <ConfirmModal onConfirm={() => {}}>
+                <Button size="sm">
+                    <Trash className="h-4 w-4" />
+                </Button>
+            </ConfirmModal>
+        </div>
+    )
+}
